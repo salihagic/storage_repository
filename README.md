9fcae92ba1e0e3a4bede57ea0a1368d63367e40e
Salih Agić <salih.agic@outlook.com>
2021-03-19 14:06:33 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Storage repository
+
+Abstraction for persisting and reading data to platform specific storage.
+
+## Usage
+```
+Future  main() async {
+final storageRepository = StorageRepository();
+//Or use like this to support dependency injection
+IStorageRepository concreteStorageRepository = StorageRepository();
+
+//init must be called
+await storageRepository.init();
+
+//or like this(if you want to separate storage by some parameter)
+await storageRepository.init(prefix: 'current_user_id');
+
+storageRepository.set('key', 'dynamic value');
+
+final value = storageRepository.get('key');
+
+bool containsKey = storageRepository.containsKey('key');
+
+storageRepository.delete('key');
+
+storageRepository.log();
+
+storageRepository.clear();
+}
+```
---
+++ lib/storage_repository.dart
@@ -0,0 +1,58 @@
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
+abstract class IStorageRepository {
+  Future init({String prefix = ''});
+  Future set<E>(dynamic key, E value);
+  E get<E>(dynamic key);
+  bool containsKey(dynamic key);
+  Future delete(dynamic key);
+  void log();
+  Future clear({String prefix = ''});
+}
+
+class StorageRepository implements IStorageRepository {
+  Box _box;
+
+  Future init({String prefix = ''}) async {
+    await Hive.initFlutter();
+    _box = await Hive.openBox('${prefix}storageBox');
+  }
+
+  @override
+  Future set<E>(dynamic key, E value) {
+    return _box.put(key, value);
+  }
+
+  @override
+  E get<E>(dynamic key) {
+    return _box.get(key);
+  }
+
+  //contains
+  @override
+  bool containsKey(dynamic key) {
+    return _box.containsKey(key);
+  }
+
+  //delete by key
+  @override
+  Future delete(dynamic key) {
+    return _box.delete(key);
+  }
+
+  void log() {
+    print('\n----------------------------------------------------------------------------------------');
+    print('Storage repository data:');
+    print('----------------------------------------------------------------------------------------');
+    _box.keys.forEach((key) {
+      print('\n\n${key.toString()}: ${_box.get(key)}');
+    });
+    print('\n----------------------------------------------------------------------------------------\n');
+  }
+
+  Future clear({String prefix = ''}) async {
+    await Hive.initFlutter();
+    await Hive.deleteBoxFromDisk('${prefix}storageBox');
+  }
+}
