f36263182cfefe056a2a463e7b705f00e00574fa
Salih Agić <salih.agic@outlook.com>
2021-09-20 13:02:06 +0200

    Hive

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+## [1.0.6] 
+* Hive
+
 ## [1.0.5] 
 * Added asString method and print renamed to log
 
--- lib/implementations/storage_repository.dart
+++ lib/implementations/storage_repository.dart
@@ -1,33 +1,63 @@
 import 'dart:developer' as developer;
 import 'dart:convert';
+import 'package:flutter/foundation.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:storage_repository/interfaces/i_storage_repository.dart';
 
 ///A basic implementation of IStorageRepository
 ///Don't use in case you want to persist some sensitive data like user tokens
 class StorageRepository implements IStorageRepository {
-  late SharedPreferences _storage;
-  static const key = 'DEFAULT_BOX';
+  late Box _storage;
+  late final String key;
+
+  StorageRepository({this.key = 'DEFAULT_BOX'});
 
   ///Method that should be called right after the
   ///initialization of an instance of this class
   @override
   Future<IStorageRepository> init() async {
-    _storage = await SharedPreferences.getInstance();
+    developer.log('INITIALIZING STORAGE REPOSITORY IN storage_repository PACKAGE');
+    await Hive.initFlutter();
+    _storage = await Hive.openBox(key);
+
+    await _copyDataFromLegacyStorage();
+
     return this;
+  }
+
+  Future _copyDataFromLegacyStorage() async {
+    final sharedPreferences = await SharedPreferences.getInstance();
+    final keys = sharedPreferences.getKeys();
+
+    if (keys.isNotEmpty) {
+      keys.forEach((key) {
+        final value = sharedPreferences.getString(key);
+        _storage.put(key, value);
+        sharedPreferences.remove(key);
+      });
+    }
   }
 
   ///Method that is used to save data to device's storage
   @override
   Future<bool> set<T>(dynamic key, T value) async {
-    return key != null && await _storage.setString(json.encode(key), json.encode(value ?? ''));
+    try {
+      await _storage.put(json.encode(key), json.encode(value ?? ''));
+
+      return true;
+    } catch (e) {
+      debugPrint('StorageRepository Exception: $e');
+    }
+    return false;
   }
 
   ///Method used to get the value saved under a given key
   @override
   Future<E?> get<E>(dynamic key) async {
     if (key == null) return null;
-    final value = _storage.getString(json.encode(key));
+    final value = _storage.get(json.encode(key));
     return value != null ? json.decode(value) : null;
   }
 
@@ -40,14 +70,27 @@ class StorageRepository implements IStorageRepository {
   ///Method that removes an item under a given key
   @override
   Future<bool> delete(dynamic key) async {
-    return key != null && await _storage.remove(json.encode(key));
+    try {
+      await _storage.delete(json.encode(key));
+
+      return true;
+    } catch (e) {
+      debugPrint('StorageRepository Exception: $e');
+    }
+    return false;
   }
 
   ///Use carefully
   ///Method that resets the storage, removes all the saved data
   @override
   Future<bool> clear() async {
-    return await _storage.clear();
+    try {
+      await _storage.clear();
+      return true;
+    } catch (e) {
+      debugPrint('StorageRepository Exception: $e');
+    }
+    return false;
   }
 
   ///Info method used for logging all the data to a console
@@ -63,8 +106,8 @@ class StorageRepository implements IStorageRepository {
     stringBuffer.write('\n----------------------------------------------------------------------------------------');
     stringBuffer.write('\nStorage repository data:');
     stringBuffer.write('\n----------------------------------------------------------------------------------------');
-    _storage.getKeys().forEach((key) {
-      stringBuffer.write('\n\n$key: ${_storage.getString(key)}');
+    _storage.keys.forEach((key) {
+      stringBuffer.write('\n\n$key: ${_storage.get(key)}');
     });
     stringBuffer.write('\n----------------------------------------------------------------------------------------');
 
